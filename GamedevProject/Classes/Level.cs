0ad70a683bb9f5d0de408d3e8da81ebcd97fcd0a
Mohammed Souleymane <Mohammedsouleymane@outlook.com>
2021-12-29 19:27:10 +0100

    update

--- GamedevProject/Classes/Level.cs
+++ GamedevProject/Classes/Level.cs
@@ -13,19 +13,19 @@ namespace GamedevProject.Classes
 {
     internal class Level
     {
-        Hero hero;
-        Monster monster;
-        List<IGameObject> objects;
-        int[,] gameboard;
+        private Hero _hero;
+        private Monster _monster;
+        private List<IGameObject> _objects;
+        private readonly int[,] gameboard;
         private Texture2D _heroTexture;
-        MovementManager movementManager;
-        Texture2D _block;
+        private MovementManager _movementManager;
+        private Texture2D _block;
         private Texture2D heart;
         public Level()
         {
 
-            objects = new List<IGameObject>();
-            movementManager = new MovementManager();
+            _objects = new List<IGameObject>();
+            _movementManager = new MovementManager();
             gameboard = new[,]
  {
                 { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -43,7 +43,7 @@ namespace GamedevProject.Classes
 
         public void Start()
         {
-            movementManager.Move(hero, objects);
+            _movementManager.Move(_hero, _objects);
         }
 
         public void AddObjects(GraphicsDevice graphicsDevice, ContentManager content)
@@ -51,41 +51,41 @@ namespace GamedevProject.Classes
             _block = new Texture2D(graphicsDevice, 1, 1);
             _block.SetData(new[] { Color.White });
             _heroTexture = content.Load<Texture2D>("Santa - Sprite Sheet");
-            hero = new Hero(_heroTexture, new KeyboardReader(), new Vector2(200, 100));
+            _hero = new Hero(_heroTexture, new KeyboardReader(), new Vector2(200, 100));
             heart = content.Load<Texture2D>("lives");
-            monster = new Monster(content);
+            _monster = new Monster(content);
             for (int i = 0; i < gameboard.GetLength(0); i++)
             {
                 for (int j = 0; j < gameboard.GetLength(1); j++)
                 {
                     if (i == 8 && j == 6)
                         Console.WriteLine();
-                    objects.Add(BlockFactory.CreateBlock((SoortBlok)gameboard[i, j], j * 50, i * 50 - 20, graphicsDevice, content));
+                    _objects.Add(BlockFactory.CreateBlock((SoortBlok)gameboard[i, j], j * 50, i * 50 - 20, graphicsDevice, content));
                 }
             }
-            objects.Add(monster);
+            _objects.Add(_monster);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            hero.Draw(spriteBatch);
-            spriteBatch.Draw(_block, hero.HitBox, Color.Red * 0.2f);
-            for (var i = 0; i < hero.Lives; i++)
+            _hero.Draw(spriteBatch);
+            spriteBatch.Draw(_block, _hero.HitBox, Color.Red * 0.2f);
+            for (var i = 0; i < _hero.Lives; i++)
             {
                 spriteBatch.Draw(heart, new Vector2(i * 30, 0), Color.White);
             }
-            foreach (var obj in objects.Where(x => x != null).ToList())
+            foreach (var obj in _objects.Where(x => x != null).ToList())
             {
                 obj.Draw(spriteBatch);
                 if (obj is Monster)
-                    spriteBatch.Draw(_block, monster.HitBox, Color.Red * 0.2f);
+                    spriteBatch.Draw(_block, _monster.HitBox, Color.Red * 0.2f);
             }
         }
 
         public void Update(GameTime gameTime)
         {
-            monster.Update(gameTime);
-            hero.Update(gameTime);
+            _monster.Update(gameTime);
+            _hero.Update(gameTime);
         }
     }
 }
